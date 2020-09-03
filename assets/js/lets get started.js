74eb5870016f0e19bb829bdf0ee411e64fde44ec
Purple Cereal <70719772+purplecereal@users.noreply.github.com>
2020-09-03 14:55:06 -0500

    updating local files
    updating local files

---
+++ assets/js/lets get started.js
@@ -0,0 +1,11 @@
+let GetStarted = document.getElementById('GetStarted')
+
+GetStarted.style.backgroundColor = '#00FFFF';
+
+
+function slide () {
+
+    document.getElementById('GetStarted')onclick.this.style.animation = "mynewmove 4s 2";
+}
+
+ 
