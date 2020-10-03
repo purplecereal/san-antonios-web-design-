ea579cd3a3ca0a0f6903c1c0502e9a15b64874ad
Vaibhav Pandey <vaibhavmay2@gmail.com>
2020-10-03 23:33:53 +0530

    scroll animation using gsap+scrollmagic

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,6 +1,39 @@
-let tl = gsap.timeline();
+var controller = new ScrollMagic.Controller();
 
+var projects = Array.prototype.slice.call(
+  document.querySelectorAll(".project")
+);
 
-tl.from(".services", {duration: 1.2, opacity: 0, scale: 0.3, ease: "back"});
+// Scenes elements
 
-tl.from(".servicesP1", {duration: 1.2, opacity: 0, scale: 0.3, ease: "back"});
+var self = this;
+
+projects.forEach(function (self) {
+  var bounce = self.querySelectorAll(".a-bounce");
+  var text = self.querySelectorAll("h2");
+
+  var sceneOne = new TimelineMax();
+
+  sceneOne.from(bounce, 3, {
+    duration: 1.2,
+    opacity: 0,
+    scale: 0.3,
+    ease: "back",
+  });
+
+  var scene1 = new ScrollMagic.Scene({
+    triggerElement: self,
+    triggerHook: "onCenter",
+    duration: 500,
+    offset: 0,
+  })
+    .setTween(sceneOne)
+    .addIndicators({
+      colorStart: "rgba(0,0,0,0.5)",
+      colorEnd: "rgba(0,0,0,0.5)",
+      colorTrigger: "rgba(0,0,0,1)",
+      name: "Scene",
+    })
+    .setClassToggle(self, "active")
+    .addTo(controller);
+});
