9e34060dec666ea3f56cabda648ffe16be6f04ef
Vaibhav Pandey <39310882+deviouscyborg@users.noreply.github.com>
2021-08-16 17:21:26 +0530

    updated contact email throught the site

--- mail_handler.php
+++ mail_handler.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "development@sanantonioswebdesign.com"; // this is your Email address
+    $to = "sanantonioswebdesign@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $subject = "Form submission";
